ccfb0b4ca2376019a80a9b2a59b22cfd1d40cded
TheBlackParrot <theblackparrot0@gmail.com>
2018-02-15 21:59:15 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,87 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+
+const settings = require("./settings.json");
+
+client.on("ready", function() {
+	console.log("ready, logged in as " + client.user.tag);
+});
+
+client.on("message", function(msg) {
+	if(msg.author.id == client.user.id) {
+		return;
+	}
+
+	var content = msg.cleanContent;
+	var prefix = content.substr(0, 1);
+
+	switch(prefix) {
+		case "+":
+			if(!(settings.allowed_channels.includes(msg.channel.name))) {
+				console.log("not allowed in " + msg.channel.name);
+				break;
+			}
+			
+			var role = content.substr(1);
+			
+			if(!(settings.allowed_roles.includes(role))) {
+				msg.reply("`" + role + "` is not a toggleable role.").then(function(reply) {
+					msg.delete({timeout: 10000});
+					reply.delete({timeout: 10000});
+				});
+				break;
+			}
+
+			var member = msg.member;
+			var roles = msg.guild.roles;
+
+			member.roles.add(roles.find("name", role));
+			msg.reply("You now have the `" + role + "` role.").then(function(reply) {
+				msg.delete({timeout: 10000});
+				reply.delete({timeout: 10000});
+			});
+
+			break;
+
+		case "-":
+			if(!(settings.allowed_channels.includes(msg.channel.name))) {
+				console.log("not allowed in " + msg.channel.name);
+				break;
+			}
+			
+			var role = content.substr(1);
+			
+			if(!(settings.allowed_roles.includes(role))) {
+				msg.reply("`" + role + "` is not a toggleable role.").then(function(reply) {
+					msg.delete({timeout: 10000});
+					reply.delete({timeout: 10000});
+				});
+				break;
+			}
+
+			var member = msg.member;
+			var roles = msg.guild.roles;
+
+			member.roles.remove(roles.find("name", role));
+			msg.reply("You no longer have the `" + role + "` role.").then(function(reply) {
+				msg.delete({timeout: 10000});
+				reply.delete({timeout: 10000});
+			});
+
+			break;
+
+		case "!":
+			break;
+
+		default:
+			/* specific case here */
+			if(msg.channel.name == "role-request") {
+				if(!(["+", "-", "!"].includes(prefix)) && msg.author.id != client.user.id) {
+					msg.delete();
+				}
+			}
+			return;
+	}
+});
+
+client.login(settings.token);
